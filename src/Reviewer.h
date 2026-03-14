670f06abf0a67537c139657e56fd0ba7d7c92bc5
João Lage <jfrlage@gmail.com>
2026-03-14 15:43:47 +0000

    Created Reviewer and Submission structs

---
+++ src/Reviewer.h
@@ -0,0 +1,15 @@
+#ifndef DA2026_PRJ1_REVIEWER_H
+#define DA2026_PRJ1_REVIEWER_H
+
+#include <string>
+using namespace std;
+
+struct Reviewer {
+    int id;
+    string name;
+    string email;
+    int primary;
+    int secondary;
+};
+
+#endif //DA2026_PRJ1_REVIEWER_H
---
+++ src/Submission.h
@@ -0,0 +1,16 @@
+#ifndef DA2026_PRJ1_SUBMISSION_H
+#define DA2026_PRJ1_SUBMISSION_H
+
+#include <string>
+using namespace std;
+
+struct Submission {
+    int id;
+    string title;
+    string authors;
+    string email;
+    int primary;
+    int secondary;
+};
+
+#endif //DA2026_PRJ1_SUBMISSION_H
