15d3c09a84456395cc166dd8439875e67c097ffa
João Lage <jfrlage@gmail.com>
2026-03-17 23:57:12 +0000

    fix: add Conference Manager header to git

---
+++ src/ConferenceManager.h
@@ -0,0 +1,34 @@
+#ifndef DA2026_PRJ1_CONFERENCEMANAGER_H
+#define DA2026_PRJ1_CONFERENCEMANAGER_H
+
+#include <unordered_map>
+#include "Submission.h"
+#include "Reviewer.h"
+#include "Parser.h"
+#include "../data-structures/Graph.h"
+
+class ConferenceManager {
+public:
+    ConferenceManager(const vector<Submission>& submissions, const vector<Reviewer>& reviewers, const Parameters& params);
+    void buildFlowGraph();
+    double edmondsKarp(int source, int sink);
+    void generateAssignments();
+    void riskAnalysis();
+
+private:
+    Graph<int> graph;
+    unordered_map<int, Submission> submissions;
+    unordered_map<int, Reviewer> reviewers;
+    Parameters params;
+    // Helpers for Node ID
+    int sourceId();
+    int sinkId();
+    int submissionNodeId(int submissionId);
+    int reviewerNodeId(int reviewerId);
+    // Helpers for Edmonds-Karp
+    bool bfs(int source, int sink, unordered_map<int, Edge<int>*>& parent);
+    double findBottleneck(int source, int sink, unordered_map<int, Edge<int>*>& parent);
+    void augmentFlow(int source, int sink, double bottleneck, unordered_map<int, Edge<int>*>& parent);
+};
+
+#endif //DA2026_PRJ1_CONFERENCEMANAGER_H
